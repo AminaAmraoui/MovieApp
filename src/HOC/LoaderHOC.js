79a28f1f76f7f010430241965dbd45b548582a99
Amina Amraoui <amraoui.amina@gmail.com>
2019-08-08 14:21:00 +0100

    Spinner HOC component

---
+++ src/HOC/LoaderHOC.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './LoaderHOC.css';
+
+const LoaderHOC =(WrappedComponent) => {
+
+    return class LoaderHOC extends Component {
+        render(){
+            return this.props.isLoading ? <div className='loader'></div> : <WrappedComponent {...this.props}/>
+        }
+    }
+}
+
+export default LoaderHOC
