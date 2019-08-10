972d568b6e777f28171c1cf1a84b72e3464e96d5
Amina Amraoui <amraoui.amina@gmail.com>
2019-08-10 10:58:45 +0100

    spinner bootstrap

--- src/HOC/LoaderHOC.js
+++ src/HOC/LoaderHOC.js
@@ -5,7 +5,11 @@ const LoaderHOC =(WrappedComponent) => {
 
     return class LoaderHOC extends Component {
         render(){
-            return this.props.isLoading ? <div className='loader'></div> : <WrappedComponent {...this.props}/>
+            return this.props.isLoading ? <div className="d-flex justify-content-center loader">
+                                                <div className="spinner-border text-info" role="status">
+                                                <span className="sr-only">Loading...</span>
+                                                </div>
+                                           </div> : <WrappedComponent {...this.props}/>
         }
     }
 }
